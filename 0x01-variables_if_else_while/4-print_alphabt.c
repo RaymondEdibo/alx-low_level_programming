adeb469b499ceb1843e61d48c19fdaf1b4f1cecb
RaymondEdibo <raymondedibo1@gmail.com>
2023-02-17 13:05:57 -0800

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,10 @@ int main(void)
 {
         char a;
 
-        a = 'a';
-
-        while (a <= 'z'){
-		if ((a != 'q' || a != 'e') && a <= 'z')
-        	putchar(a);
-        	a++;
-        }
-
-        putchar('\n');
-        return (0);
+	for (a = 'a', a <= 'z' && a != 'q' && a != 'e', a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
 }
