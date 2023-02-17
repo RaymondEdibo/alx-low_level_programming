5fa45d170ebb81f42af7fbb2fe41963bf80afb70
RaymondEdibo <raymondedibo1@gmail.com>
2023-02-17 13:32:43 -0800

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-	for (c ='a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
 			putchar(c);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
