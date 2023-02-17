cd8597edb244443c52fc7da70dc8dc05fd4f8b79
RaymondEdibo <raymondedibo1@gmail.com>
2023-02-17 08:51:29 -0800

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-	char A
+	char A;
 
-	for (A = 'a'; A <= 'z'; A++)
+	for (A = 'a'; A <= 'z'; A++);
 	putchar(A);
 
-	for (A = 'A'; A <= 'Z'; A++)
+	for (A = 'A'; A <= 'Z'; A++);
 	putchar(A);
 	putchar('\n');
 return (0);
