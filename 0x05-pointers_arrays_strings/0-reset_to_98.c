0238ca378272c797cca7b409e81b83069a57eb38
RaymondEdibo <raymondedibo1@gmail.com>
2023-02-27 14:01:21 -0800

    c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that takes a pointer and updates value
+ * @n: input update
+ *
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
