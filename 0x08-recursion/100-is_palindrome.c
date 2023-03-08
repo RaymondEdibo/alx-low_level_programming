197213040b501845ea31d228dea48eb90d440df8
RaymondEdibo <raymondedibo1@gmail.com>
2023-03-08 09:27:25 -0800

    c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,54 @@
 #include "main.h"
 
-int check_palindrome(char *s);
-
 /**
-  * is_palindrome - Returns if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int is_palindrome(char *s)
+ * long_1 - check the code for Holberton School students.
+ * @s: number
+ *
+ * Return: Always 0.
+ */
+
+int long_1(char *s)
 {
-	if (*s == '0')
-		return (1);
-
-	return (check_palindrome(s));
-}
-
-/**
-  * check_palindrome - Check if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
+	if (*s != '\0')
 	{
-		s++;
-		l--;
+		return (1 + long_1(s + 1));
+	}
+	return (0);
+
+}
+/**
+ * compare - check the code for Holberton School students.
+ * @s: number
+ * @l: lenght
+ *
+ * Return: Always 0.
+ */
+int compare(char *s, int l)
+{
+	if (l <= 0)
+	{
+		return (1);
+	}
+	if (*s == *(s + (l - 1)))
+	{
+		return (compare(s + 1, l - 2));
 	}
 	else
 	{
 		return (0);
 	}
-
-	return (1);
 }
 
 /**
-  * _strlen_recursion - Get the length of a string
-  * @s: the string to get the length
-  *
-  * Return: the string length
-  */
-int _strlen_recursion(char *s)
+ * is_palindrome - check the code for Holberton School students.
+ * @s: character
+ *
+ * Return: Always 0.
+ */
+int is_palindrome(char *s)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
+	int l;
 
-	s++;
-	return (_strlen_recursion(s) + 1);
+	l = long_1(s);
+	return (compare(s, l));
 }
