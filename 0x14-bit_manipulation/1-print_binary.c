2fab4187ecad3d2392064c3ff1466864ddd71ad4
RaymondEdibo <raymondedibo1@gmail.com>
2023-04-08 10:14:57 -0700

    c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_binary - print
+*
+* @n: number
+*/
+
+void print_binary(unsigned long int n){
+
+int l, k, z, r;
+z = n/1000,
+l = n/100;
+k = n/10;
+h = n%10;
+
+
+r = ((z*8)+(l*4)+(k*2)+(h*1));
+	_putchar(r + '0');
+}
