b79644dbf37dec8eaf07a44e9fb7d2889b54d8dd
RaymondEdibo <raymondedibo1@gmail.com>
2023-04-10 15:29:35 -0700

    c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
+#include <math.h>
 
 /**
   * print_binary - Prints
@@ -12,28 +13,14 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
-
-	_do(n);
-}
-
-/**
-  * _do - print
-  * @n: integer
-  *
-  * Return: 0
-  */
-void _divide(unsigned long int n)
-{
-	if (n < 1)
+	else if (n == 1)
+	{
+		putchar ('1');
 		return;
-
-	_do(n >> 1);
-
-	if (n & 1)
-		_putchar('1');
-	else
-		_putchar('0');
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
