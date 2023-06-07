2357e1d5138aafe508a8d27098a0cbf5feed998a
RaymondEdibo <raymondedibo1@gmail.com>
2023-06-07 14:02:51 -0700

    work

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - reverse
+ * @s: input
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+	else
+		return;
+	_putchar(*s);
+}
