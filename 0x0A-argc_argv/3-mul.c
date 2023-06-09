2ff3a7cc6a80058593d18bb40bb4354bb8f190f0
RaymondEdibo <raymondedibo1@gmail.com>
2023-06-09 08:26:25 -0700

    work

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,17 @@ int main(int argc, char *argv[])
 {
 	int i, j;
 
-	i = atoi(argv[argc - 2]);
-	j = atoi(argv[argc - 1]);
-	printf("%d\n", i * j);
-	return (0);
+	if (argc == 3)
+	{
+		i = atoi(argv[argc - 2]);
+		j = atoi(argv[argc - 1]);
+		printf("%d\n", i * j);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+
+	}
 }
