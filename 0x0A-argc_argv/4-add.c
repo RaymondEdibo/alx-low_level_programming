35d8f50e21c1097e183df2ebac6b5f89a535f214
RaymondEdibo <raymondedibo1@gmail.com>
2023-06-09 09:01:31 -0700

    work

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  *main - check the code
@@ -10,3 +12,25 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, j;
+	int add = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+		printf("%d\n", add);
+		return (0);
+}
