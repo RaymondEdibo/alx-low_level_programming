5f07a6e31851ccc11d8a6f8bf47b06428250d298
RaymondEdibo <raymondedibo1@gmail.com>
2023-06-15 13:42:39 -0700

    work

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,20 +8,21 @@
  *
  *Return: 0
  */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *a;
+	void *a;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	a = malloc(nmemb * size);
+	i = nmemb * size;
+
+	a = malloc(i);
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		a[i] = 0;
+	memset(a, 0, i);
 
 	return (a);
 }
