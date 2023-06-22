a9edccbeebdb98d2fa490cb9b147948c106bb98b
RaymondEdibo <raymondedibo1@gmail.com>
2023-06-22 12:58:48 -0700

    work

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -7,4 +8,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct check - Data type
+ * @op: check.
+ * @f: Function.
+ *
+ */
+
+typedef struct check
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
 #endif
