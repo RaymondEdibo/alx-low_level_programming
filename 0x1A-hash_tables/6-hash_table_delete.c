e3f553ccd84bf77784b68381e6ea4de97a8c066d
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-09-07 23:08:55 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,23 +1,23 @@
 #include "hash_tables.h"
 /**
-* hash_table_delete - Delete hash table
-* @ht: Hash table
-* Return: deleted hash table
+* hash_table_delete - Delete the hash table
+* @ht: the hash table
+* Return: the hash table deleted
 */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp, *nxt;
+	hash_node_t *temporal, *next;
 	unsigned long int index;
 
 	if (!ht)
 		return;
 	for (index = 0; index < ht->size; index++)
-		for (temp = ht->array[index]; temp; temp = nxt)
+		for (temporal = ht->array[index]; temporal; temporal = next)
 		{
-			nxt = temp->nxt;
-			free(temp->key);
-			free(temp->value);
-			free(temp);
+			next = temporal->next;
+			free(temporal->key);
+			free(temporal->value);
+			free(temporal);
 		}
 	free(ht->array);
 	free(ht);
